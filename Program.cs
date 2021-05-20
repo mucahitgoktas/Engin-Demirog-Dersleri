65618b458f011929c35c07044a618449f9797b11
mucahitgoktas <mucahitgoktas@gmail.com>
2021-05-20 20:44:20 +0300

    Engin Demirog Dersleri
    20 Mayıs Gelinen Nokta :  While Döngüsü

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Engin_Demirog_Dersleri
+{
+    class Program
+    {
+        /*static void Main(string[] args)   /// FOR DÖNGÜSÜ  
+        {
+            for (int i = 0; i <= 100; i++) 
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }*/
+        /*static void Main(string[] args)   /// WHİLE
+        {
+            int number = 100;
+            while (number > 0)
+            {
+                Console.WriteLine(number);
+                number--;
+
+            }
+            Console.WriteLine($"now number is : {number}");
+            Console.ReadLine();
+
+        }*/
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}

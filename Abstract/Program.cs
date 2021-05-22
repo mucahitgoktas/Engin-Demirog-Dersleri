f69414dd4367e72fc53e7c8e0c9c41c9c6bf8299
mucahitgoktas <mucahitgoktas@gmail.com>
2021-05-23 00:39:39 +0300

    Abstract'lar bitti

---
+++ Abstract/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database database = new SqlServer(); // abstractlar new'lenemez!! bunun yerine eşitliğin karşısına, alt sınıf (oğul) new'lenebilir. birkaç farklı oğul varsa herbiri ayrı değişkene atanır.
+            Console.WriteLine(database);
+            Console.ReadLine();
+        }
+    }
+
+    // abstracklar inheritens özelliği taşır ancak, virtual methodlar gibi bazı özel durumlar için kullanılır. 
+    // örneğin bir metod (add) her class için aynıysa ancak (Delete) her metot için farklı olacaksa, abstractlardan yararlanılır.
+
+
+    abstract class Database 
+    {
+        public void Add()
+        {
+            Console.WriteLine("Added");
+        }
+
+        public abstract void Delete();
+    }
+
+    class SqlServer : Database
+    {
+        public override void Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

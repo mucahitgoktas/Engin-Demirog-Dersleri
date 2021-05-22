6e0d89c22255a63b4d2ffd59fe25cb3725292132
mucahitgoktas <mucahitgoktas@gmail.com>
2021-05-22 15:35:33 +0300

    Interface'ler giriş

---
+++ Interfaces/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.ReadLine();
+
+
+        }
+        interface IPerson
+        {
+            int Id { get; set; }
+            string FirstName { get; set; }
+            string LastName { get; set; }
+        }
+
+        class Customer : IPerson
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public string Address { get; set; }
+        }
+
+        class Student : IPerson
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public string Departmant { get; set; }
+        }
+
+        class Worker : IPerson
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public string Departmant { get; set; }
+        }
+
+        class PersonManager
+        {
+            public void Add(IPerson person)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+        }
+
+    }
+}
